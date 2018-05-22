f5a00894f7e3a0c3acdb78fbd81be1ff67fe8eb1
Алексей Дурягин <aleksey.duryagin.n@yandex.ru>
2018-05-22 22:23:58 +0300

    d

--- ConsoleUtility/src/TestTar.java
+++ ConsoleUtility/src/TestTar.java
@@ -15,11 +15,11 @@ class TestTar {
 	@Test
 	void out() throws IOException {
 		
-		String[] inputPaths = {"src/input/i.txt",
-				"src/input/n.txt",
-				"src/input/put.txt"};
+		String[] inputPaths = {"C:\\repo\\ConsoleUtility\\src\\input\\i.txt",
+				"C:\\repo\\ConsoleUtility\\src\\input\\n.txt",
+				"C:\\repo\\ConsoleUtility\\src\\input\\put.txt"};
 		String[] names = {"i.txt", "n.txt", "put.txt"};
-		String output = "src/output/output.txt";
+		String output = "C:\\repo\\ConsoleUtility\\src\\output\\output.txt";
 		
 		Tar.out(inputPaths, output);
 		
@@ -50,11 +50,10 @@ class TestTar {
 		bufferedReader.close();
 	}
 	
-	
 	@Test
 	void u() throws IOException {
 		
-		String input = "src/output/output.txt";
+		String input = "C:\\repo\\ConsoleUtility\\src\\output\\output.txt";
 		String[] names = {"i.txt", "n.txt", "put.txt"};
 		
 		File inputFile = new File(input);
